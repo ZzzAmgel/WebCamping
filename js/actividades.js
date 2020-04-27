fdb02b6a46450fb193c1d16094751200203bb887
ZzzAmgel <elangelmiranda@gmail.com>
2020-04-27 17:13:10 +0200

    Nuevo diseño

--- js/actividades.js
+++ js/actividades.js
@@ -1,37 +1,36 @@
+var rootRef = firebase.database().ref().child("uploads");
 
-    var rootRef = firebase.database().ref().child("uploads");
+function htmlEscape(str) {
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
 
-    function htmlEscape(str) {
-        return String(str)
-                .replace(/&/g, '&amp;')
-                .replace(/"/g, '&quot;')
-                .replace(/'/g, '&#39;')
-                .replace(/</g, '&lt;')
-                .replace(/>/g, '&gt;');
-    }
+rootRef.on("child_added", snap => {
 
-    rootRef.on("child_added", snap => {
-        
-        var name = snap.child("mName").val();
-        var url = snap.child("mImageUrl").val();
-        console.log("test");
+    var name = snap.child("mName").val();
+    var url = snap.child("mImageUrl").val();
+    console.log("test");
 
-        
 
-        $("#table_body").append(`
+
+    $("#table_body").append(`
         
         <div class="col-md-3">
         <div class="card">
-            <img class="card-img-top" alt="Bootstrap Thumbnail First" style="max-height : 150px" src="${url}" />
+            <img class="card-img-top" alt="Bootstrap Thumbnail First" style="object-fit: cover; max-height : 150px" src="${url}" />
             <div class="card-block" style="word-break : break-all;">
                 <h4>${name.replace(/\n/g, "<br />")}</h4>
             </div>
         </div>
     </div>    
     `);
-    });
+});
 
-    
+
 
 
 /*
@@ -52,5 +51,3 @@
                         </div>
                     </div>
                     */
-
-
--- js/reservas.js
+++ js/reservas.js
@@ -1,35 +1,33 @@
-
-
 observador();
 
-function observador(){
-    firebase.auth().onAuthStateChanged(function(user) {
-        
-        if (user) {
-          // User is signed in.
-          console.log('Existe usuario activo');
-          console.log(user);
-          var displayName = user.displayName;
-          var email = user.email;
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
+function observador() {
+  firebase.auth().onAuthStateChanged(function (user) {
 
-          var contenido = document.getElementById('btnshow');
-  if(user.emailVerified){
-  contenido.innerHTML = `
+    if (user) {
+      // User is signed in.
+      console.log('Existe usuario activo');
+      console.log(user);
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+
+      var contenido = document.getElementById('btnshow');
+      if (user.emailVerified) {
+        contenido.innerHTML = `
     <p>Bienvenido</p>
     <button onclick="writeUserData()">Enviar</button>`;
-  }
-          // ...
-        } else {
-            contenido.innerHTML = `
+      }
+      // ...
+    } else {
+      contenido.innerHTML = `
             <p>Debe verificar su dirección de correo electrónico</p>`;
-          console.log('No existe usuario activo');
-        }
-      });
+      console.log('No existe usuario activo');
+    }
+  });
 }
 
 
@@ -37,33 +35,52 @@ function observador(){
 
 function writeUserData() {
 
-    var nombrereserva = document.getElementById('nombrereserva').value;
-    var dni = document.getElementById('dni').value;
-    var fechaentrada = document.getElementById('fechaentrada').value;
-    var fechasalida = document.getElementById('fechasalida').value;
-    var numeroadultos = document.getElementById('numeroadultos').value;
-    var numeroninos = document.getElementById('numeroninos').value;
-    var numerotelefono = document.getElementById('numerotelefono').value;
-    var casa = "N/C"
-    alert("Yeee");
-    console.log(numeroadultos);
-    firebase.auth().onAuthStateChanged(function(user) {
+  var nombrereserva = document.getElementById('nombrereserva').value;
+  var dni = document.getElementById('dni').value;
+  var fechaentrada = document.getElementById('fechaentrada').value;
+  var fechasalida = document.getElementById('fechasalida').value;
+  var numeroadultos = document.getElementById('numeroadultos').value;
+  var numeroninos = document.getElementById('numeroninos').value;
+  var numerotelefono = document.getElementById('numerotelefono').value;
+  var casa = "N/C"
 
-        var email = user.email;
-    
-    
-    
-    firebase.database().ref('/Reservas').push({
-        Titulo : nombrereserva,
-        DNI : dni,
-        FechaInicio : fechaentrada,
-        FechaFin : fechasalida,
-        NumAdultos : numeroadultos,
-        NumNinos : numeroninos,
-        NumeroTelefono : numerotelefono,
-        Casa : casa,
-        Email : email
-        
+  if (nombrereserva == "" || dni == "" || fechaentrada == "" || fechasalida == "" || numeroadultos == "" || numerotelefono == "") {
+    toastr["warning"]('Alguno de los campos esta vacio, compruebalos.', {
+      "newestOnTop": true,
+      "progressBar": true,
+      "positionClass": "toast-top-right",
+      "preventDuplicates": false,
+      "onclick": null,
+      "showDuration": "300",
+      "hideDuration": "1000",
+      "timeOut": "5000",
+      "extendedTimeOut": "1000",
+      "showEasing": "swing",
+      "hideEasing": "linear",
+      "showMethod": "fadeIn",
+      "hideMethod": "fadeOut"
     });
-});
+    return 0;
   }
+
+  console.log(numeroadultos);
+  firebase.auth().onAuthStateChanged(function (user) {
+
+    var email = user.email;
+
+
+
+    firebase.database().ref('/Reservas').push({
+      Titulo: nombrereserva,
+      DNI: dni,
+      FechaInicio: fechaentrada,
+      FechaFin: fechasalida,
+      NumAdultos: numeroadultos,
+      NumNinos: numeroninos,
+      NumeroTelefono: numerotelefono,
+      Casa: casa,
+      Email: email
+    });
+    toastr["sucess"]('Su pre reserva se ha enviado con exito.');
+  });
+}
